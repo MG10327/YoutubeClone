2efe7f3589832a22d9fbb7f678f59f4623eafd08
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-22 11:39:55 -0400

    basic sidebar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home/home.jsx'
+import Video from './pages/video/video.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/video/:categoryId/:videoId" element={<Video/>} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sidebar.scss'
+
+const sidebar = () => {
+  return (
+    <div>sidebar</div>
+  )
+}
+
+export default sidebar
