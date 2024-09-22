8d541d5edfcf5ec935b12edd04267ab07bf9bc22
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-22 18:38:03 -0400

    wrap up recommended section styles

--- src/pages/video/video.jsx
+++ src/pages/video/video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './video.scss'
 import PlayVideo from '../../components/playvideo/playvideo'
+import Recommended from '../../components/recommended/recommended'
 
 const video = () => {
   return (
     <div className='play-container'>
         <PlayVideo />
+        <Recommended />
     </div>
   )
 }
