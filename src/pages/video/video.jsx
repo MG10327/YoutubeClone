cefba942d1d50100609872dfb21dd981c5fb1739
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-22 16:30:56 -0400

    video play page basic setup

--- src/pages/video/video.jsx
+++ src/pages/video/video.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './video.scss'
+import PlayVideo from '../../components/playvideo/playvideo'
 
 const video = () => {
   return (
-    <div></div>
+    <div className='play-container'>
+        <PlayVideo />
+    </div>
   )
 }
 
