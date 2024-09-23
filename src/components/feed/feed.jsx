3503ca6d12b850ac87d2ca129efa7c2354bb8b11
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-22 21:08:17 -0400

    display when videos were uploaded

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,15 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './feed.scss'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom'
 import { API_KEY, value_converter } from '../../data'
+import moment from 'moment'
 
 const feed = ({category}) => {
 
@@ -32,7 +25,7 @@ const feed = ({category}) => {
                     <img src={item.snippet.thumbnails.medium.url} alt="" />
                     <h2>{item.snippet.title}</h2>
                     <h3>{item.snippet.channelTitle}</h3>
-                    <p>{value_converter(item.statistics.viewCount)} Views </p>
+                    <p>{value_converter(item.statistics.viewCount)} Views &bull; {moment(item.snippet.publishedAt).fromNow()} </p>
                 </Link>
             )
         })}
